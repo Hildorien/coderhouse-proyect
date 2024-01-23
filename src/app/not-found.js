49054384c83c889efc0b2ee482a100b2cd25773b
Nacho <nachofernandez.1995@hotmail.com>
2024-01-23 10:41:22 -0300

    Update default language in NotFound component

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,10 +1,12 @@
 import GoBack from '@/app/components/ui/buttons/GoBack';
 import { getDictionary } from '@/dictionaries';
 
+/* NotFound components do not accept any props -> We can't internationalize it
+Source https://nextjs.org/docs/app/api-reference/file-conventions/not-found */
 export default async function NotFound() {
 
     // At this level of the app we can't detect the languague of the client so we need to use the default language
-    const t = await getDictionary('en');
+    const t = await getDictionary('es');
 
     return (
         <div className="container m-auto">
