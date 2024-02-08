37c45954ff59f698f0db1bc0dffdc215ac74c7c6
Nacho <nachofernandez.1995@hotmail.com>
2024-02-08 20:04:04 -0300

    Remove translationService.js

--- src/app/service/translation/translationService.js
+++
@@ -1,9 +0,0 @@
-export async function fetchTranslation(lang) {
-    const products = await fetch(
-        `http://localhost:3000/api/translation/${lang}`,
-        {
-            cache: "force-cache"
-        },
-    ).then((res) => res.json());
-    return products;
-}
